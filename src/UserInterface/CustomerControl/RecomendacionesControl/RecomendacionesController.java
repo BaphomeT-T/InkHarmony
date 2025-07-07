a3a5545d20fc5aee292b0a1c7ac446b23d47533c
LeonelXD1 <leonel.onacevallos@epn.edu.ec>
2025-07-07 16:05:08 -0500

    Se agrega controlador para las recomendaciones

---
+++ src/UserInterface/CustomerControl/RecomendacionesControl/RecomendacionesController.java
@@ -0,0 +1,39 @@
+package UserInterface.CustomerControl.RecomendacionesControl;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class RecomendacionesController {
+
+    @FXML private Button btnPlaylist;
+    @FXML private Button btnGenero;
+    @FXML private Button btnPersonalizadas;
+    @FXML private Button btnEstrenos;
+
+    @FXML private StackPane contentPane;
+
+    @FXML
+    public void initialize() {
+        // Acciones de los botones
+        btnPlaylist.setOnAction(e -> cargarVista("playlist.fxml"));
+        btnGenero.setOnAction(e -> cargarVista("porGenero.fxml"));
+        btnPersonalizadas.setOnAction(e -> cargarVista("personalizadas.fxml"));
+        btnEstrenos.setOnAction(e -> cargarVista("estrenos.fxml"));
+    }
+
+    private void cargarVista(String fxml) {
+        try {
+            AnchorPane vista = FXMLLoader.load(getClass().getResource("UserInterface/GUI/recomendaciones" + fxml));
+
+            contentPane.getChildren().setAll(vista);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
