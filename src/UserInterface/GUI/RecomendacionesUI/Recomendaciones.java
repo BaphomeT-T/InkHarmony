014a5bfb24bdbae83f1f69dd9e8fa39212c62fcf
LeonelXD1 <leonel.onacevallos@epn.edu.ec>
2025-07-07 21:03:49 -0500

    pa ejecutar

---
+++ src/UserInterface/GUI/RecomendacionesUI/Recomendaciones.java
@@ -0,0 +1,26 @@
+package UserInterface.GUI.RecomendacionesUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class Recomendaciones extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Cargar el FXML
+        Parent root = FXMLLoader.load(getClass().getResource(
+                "/UserInterface/GUI/RecomendacionesUI/recomendaciones.fxml"));
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Administración de Usuarios");
+        primaryStage.setScene(scene);
+
+        // Establecer tamaño mínimo (para que no se pueda achicar más)
+        primaryStage.setMinWidth(1280);
+        primaryStage.setMinHeight(680);
+
+        primaryStage.show();
+    }
+}
