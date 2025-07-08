6142387f16a96d803f16865977c1907cb70fa2da
LeonelXD1 <leonel.onacevallos@epn.edu.ec>
2025-07-07 21:03:49 -0500

    pa ejecutar

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import UserInterface.GUI.RecomendacionesUI.Recomendaciones;
+import javafx.application.Application;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Application.launch(Recomendaciones.class, args);
     }
 }
