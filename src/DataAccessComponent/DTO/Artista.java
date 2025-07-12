3f5a62a63cd041ec8b4a73d66e541708548f0596
Katiuska <alison.lita@epn.edu.ec>
2025-07-12 01:35:11 -0500

    pantalla de subir artista,controlador, artistaDAO y artista

--- src/DataAccessComponent/DTO/Artista.java
+++ src/DataAccessComponent/DTO/Artista.java
@@ -9,10 +9,49 @@ public class Artista {
     //Generos debemos coordinar
     //protected Genero genero; creo que así porque es enum en el modulo de catálogo canciones
     private String genero;
-    private Date fechaNacimiento;
+
     private String biografia;
 
-    //Coordinar con los otros grupos
     private String imagen;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBiografia(String biografia) {
+        this.biografia = biografia;
+    }
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+
+    public void setGeneros(String genero) {
+        this.genero = genero;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
 }
