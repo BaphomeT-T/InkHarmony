0e9be9c568b1790db9add959647b7a640a455a21
domdume <domingacf@gmail.com>
2025-07-19 23:18:08 -0500

    GUI para editar artista

---
+++ src/UserInterface/CustomerControl/CatalogoArtistas/EditarArtistasController.java
@@ -0,0 +1,8 @@
+package UserInterface.CustomerControl.CatalogoArtistas;
+
+import javafx.event.ActionEvent;
+
+public class EditarArtistasController {
+    public void editarArtista(ActionEvent actionEvent) {
+    }
+}
