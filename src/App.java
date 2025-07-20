711afafcfa41cace5c7c5db7d5df6615a54a8d82
Danna <dannamo2005@gmail.com>
2025-07-20 01:49:01 -0500

    Pantalla CatalogoArtista finalizada (se agrego css para diseño del TableView), conexión entre pantallas y prueba de busqueda, agregar y eliminar.

--- src/App.java
+++ src/App.java
@@ -8,7 +8,7 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/UserInterface/GUI/CatalogoArtistas/PantallaSubirArtista.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/UserInterface/GUI/CatalogoArtistas/PantallaCatalogoArtista.fxml"));
             Parent root = loader.load();
 
             Scene scene = new Scene(root);
