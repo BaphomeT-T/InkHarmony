a97ddd3df453efbf46aa8f0e43509996d78ed844
SnowPoom <jonathan.pagual@hotmail.com>
2025-07-31 12:04:54 -0500

    PreMerge Reproductor

--- scriptsBD/DDL_InkHarmony.sql
+++ scriptsBD/DDL_InkHarmony.sql
@@ -1,14 +1,15 @@
 -- database: ../database/InkHarmony.sqlite
 -- Borrar tablas en orden seguro
 DROP TABLE IF EXISTS Reproduccion;
-DROP TABLE IF EXISTS Playlist_Cancion;
+DROP TABLE IF EXISTS playlists;
+DROP TABLE IF EXISTS playlist_elementos;
 DROP TABLE IF EXISTS Cancion_Genero;
 DROP TABLE IF EXISTS Cancion_Artista;
 DROP TABLE IF EXISTS Artista_Genero;
 DROP TABLE IF EXISTS Playlist;
 DROP TABLE IF EXISTS Cancion;
 DROP TABLE IF EXISTS Artista;
--- DROP TABLE IF EXISTS Genero;
+ DROP TABLE IF EXISTS Genero;
 DROP TABLE IF EXISTS Usuario;
 
 
@@ -26,29 +27,29 @@ CREATE TABLE Usuario (
                          tipo_usuario VARCHAR(20) NOT NULL
 );
 
--- Tabla Genero
--- CREATE TABLE Genero (
---                         id_genero INTEGER PRIMARY KEY AUTOINCREMENT,
---                         nombre_genero VARCHAR(20) NOT NULL
--- );
+--Tabla Genero
+CREATE TABLE Genero (
+                        id_genero INTEGER PRIMARY KEY AUTOINCREMENT,
+                        nombre_genero VARCHAR(20) NOT NULL
+);
 
 -- Tabla Artista
-CREATE TABLE Artista (
-                         id_artista INTEGER PRIMARY KEY AUTOINCREMENT,
-                         nombre VARCHAR(20) NOT NULL,
-                         nacionalidad VARCHAR(20),
-                         biografia VARCHAR(20),
-                         fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+    CREATE TABLE Artista (
+                             id_artista INTEGER PRIMARY KEY AUTOINCREMENT,
+                             nombre VARCHAR(50) NOT NULL UNIQUE,
+                             biografia TEXT NOT NULL,
+                             imagen BLOB -- o BLOB si quieres guardar bytes
+    );
 
--- Tabla Artista_Genero (N:M)
-CREATE TABLE Artista_Genero (
-                                id_artista INTEGER NOT NULL,
-                                id_genero INTEGER NOT NULL,
-                                PRIMARY KEY (id_artista, id_genero),
-                                FOREIGN KEY (id_artista) REFERENCES Artista(id_artista),
-                                FOREIGN KEY (id_genero) REFERENCES Genero(id_genero)
-);
+
+    -- Tabla Artista_Genero (N:M)
+    CREATE TABLE Artista_Genero (
+                                    id_artista INTEGER NOT NULL,
+                                    id_genero INTEGER NOT NULL,
+                                    PRIMARY KEY (id_artista, id_genero),
+                                    FOREIGN KEY (id_artista) REFERENCES Artista(id_artista),
+                                    FOREIGN KEY (id_genero) REFERENCES Genero(id_genero)
+    );
 
 -- Tabla Cancion
 CREATE TABLE Cancion (
@@ -106,113 +107,4 @@ CREATE TABLE Reproduccion (
                               fecha_hora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
                               FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
                               FOREIGN KEY (id_cancion) REFERENCES Cancion(id_cancion)
-<<<<<<< HEAD
 );
-    -- Borrar tablas en orden seguro
-    DROP TABLE IF EXISTS Reproduccion;
-    DROP TABLE IF EXISTS Playlist_Cancion;
-    DROP TABLE IF EXISTS Cancion_Genero;
-    DROP TABLE IF EXISTS Cancion_Artista;
-    DROP TABLE IF EXISTS Artista_Genero;
-    DROP TABLE IF EXISTS Playlist;
-    DROP TABLE IF EXISTS Cancion;
-    DROP TABLE IF EXISTS Artista;
-    DROP TABLE IF EXISTS Genero;
-    DROP TABLE IF EXISTS Usuario;
-
-
-    -- Tabla Usuario
-    CREATE TABLE Usuario (
-                             id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
-                             nombre_usuario VARCHAR(20) NOT NULL,
-                             apellido_usuario VARCHAR(20),
-                             correo VARCHAR(20) UNIQUE NOT NULL,
-                             contraseña VARCHAR(20) NOT NULL,
-                             fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                             tipo_usuario BOOLEAN NOT NULL DEFAULT 0
-    );
-
-    -- Tabla Genero
-    CREATE TABLE Genero (
-                            id_genero INTEGER PRIMARY KEY AUTOINCREMENT,
-                            nombre_genero VARCHAR(20) NOT NULL
-    );
-
-    -- Tabla Artista
-    CREATE TABLE Artista (
-                             id_artista INTEGER PRIMARY KEY AUTOINCREMENT,
-                             nombre VARCHAR(50) NOT NULL UNIQUE,
-                             biografia TEXT NOT NULL,
-                             imagen BLOB -- o BLOB si quieres guardar bytes
-    );
-
-
-    -- Tabla Artista_Genero (N:M)
-    CREATE TABLE Artista_Genero (
-                                    id_artista INTEGER NOT NULL,
-                                    id_genero INTEGER NOT NULL,
-                                    PRIMARY KEY (id_artista, id_genero),
-                                    FOREIGN KEY (id_artista) REFERENCES Artista(id_artista),
-                                    FOREIGN KEY (id_genero) REFERENCES Genero(id_genero)
-    );
-
-    -- Tabla Cancion
-    CREATE TABLE Cancion (
-                             id_cancion INTEGER PRIMARY KEY AUTOINCREMENT,
-                             anio INTEGER,
-                             titulo VARCHAR(20) NOT NULL,
-                             archivo_mp3 BLOB,
-                             duracion REAL,
-                             fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                             portada BLOB
-    );
-
-    -- Tabla Cancion_Genero (N:M)
-    CREATE TABLE Cancion_Genero (
-                                    id_cancion INTEGER NOT NULL,
-                                    id_genero INTEGER NOT NULL,
-                                    PRIMARY KEY (id_cancion, id_genero),
-                                    FOREIGN KEY (id_cancion) REFERENCES Cancion(id_cancion),
-                                    FOREIGN KEY (id_genero) REFERENCES Genero(id_genero)
-    );
-
-    -- Tabla Cancion_Artista (N:M)
-    CREATE TABLE Cancion_Artista (
-                                     id_cancion INTEGER NOT NULL,
-                                     id_artista INTEGER NOT NULL,
-                                     PRIMARY KEY (id_cancion, id_artista),
-                                     FOREIGN KEY (id_cancion) REFERENCES Cancion(id_cancion),
-                                     FOREIGN KEY (id_artista) REFERENCES Artista(id_artista)
-    );
-
-    -- Tabla Playlist
-    CREATE TABLE Playlist (
-                              id_playlist INTEGER PRIMARY KEY AUTOINCREMENT,
-                              id_usuario INTEGER,
-                              nombre VARCHAR(20) NOT NULL,
-                              fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                              descripcion VARCHAR(20),
-                              FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
-    );
-
-    -- Tabla Playlist_Cancion (N:M)
-    CREATE TABLE Playlist_Cancion (
-                                      id_playlist INTEGER NOT NULL,
-                                      id_cancion INTEGER NOT NULL,
-                                      PRIMARY KEY (id_playlist, id_cancion),
-                                      FOREIGN KEY (id_playlist) REFERENCES Playlist(id_playlist),
-                                      FOREIGN KEY (id_cancion) REFERENCES Cancion(id_cancion)
-    );
-
-    -- Tabla Reproduccion
-    CREATE TABLE Reproduccion (
-                                  id_reproduccion INTEGER PRIMARY KEY AUTOINCREMENT,
-                                  id_usuario INTEGER,
-                                  id_cancion INTEGER,
-                                  fecha_hora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                  FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
-                                  FOREIGN KEY (id_cancion) REFERENCES Cancion(id_cancion)
-    );
-=======
-);
->>>>>>> adcb48c002832e4c9e16b0560b19b794d3ea2ec7
--- scriptsBD/DML_InkHarmony.sql
+++ scriptsBD/DML_InkHarmony.sql
@@ -1,25 +1,24 @@
 -- database: ../database/InkHarmony.sqlite
 
-<<<<<<< HEAD
--- INSERT INTO Genero (nombre_genero) VALUES
---                                        ('GAME_OST'),
---                                        ('ROCK'),
---                                        ('ROCK_ALTERNATIVO'),
---                                        ('VALS'),
---                                        ('TANGO'),
---                                        ('REGIONAL_MEXICANA'),
---                                        ('RAP'),
---                                        ('CLASICA'),
---                                        ('MOVIE_OST'),
---                                        ('JAZZ'),
---                                        ('COUNTRY'),
---                                        ('POP'),
---                                        ('REGGAETON'),
---                                        ('TRAP_LATINO'),
---                                        ('TRAP'),
---                                        ('K_POP'),
---                                        ('BOLERO'),
---                                        ('PASILLO');
+ INSERT INTO Genero (nombre_genero) VALUES
+                                        ('GAME_OST'),
+                                        ('ROCK'),
+                                        ('ROCK_ALTERNATIVO'),
+                                        ('VALS'),
+                                        ('TANGO'),
+                                        ('REGIONAL_MEXICANA'),
+                                        ('RAP'),
+                                        ('CLASICA'),
+                                        ('MOVIE_OST'),
+                                        ('JAZZ'),
+                                        ('COUNTRY'),
+                                        ('POP'),
+                                        ('REGGAETON'),
+                                        ('TRAP_LATINO'),
+                                        ('TRAP'),
+                                        ('K_POP'),
+                                        ('BOLERO'),
+                                        ('PASILLO');
 INSERT INTO Usuario (nombre_usuario, apellido_usuario, correo, contraseña, tipo_usuario, id_foto_Perfil, estado_cuenta) VALUES
 ('Jonas', 'Garcia', 'jonas@gmail.com', '$2a$10$wFBV8MmGWPzIwsnhJ3/w0eWNici2F8dXx1RUM7F0TxTAa1T2CWfPK', 'ADMINISTRADOR',  '1', 'activo');
 SELECT * FROM Usuario;
@@ -27,35 +26,6 @@ UPDATE Usuario
                     SET  estado_cuenta= "activo"
                     WHERE correo = "jonas@gmail.com";
 
-
-
-
-
-
-
-
-=======
->>>>>>> adcb48c002832e4c9e16b0560b19b794d3ea2ec7
-INSERT INTO Genero (nombre_genero) VALUES
-                                       ('GAME_OST'),
-                                       ('ROCK'),
-                                       ('ROCK_ALTERNATIVO'),
-                                       ('VALS'),
-                                       ('TANGO'),
-                                       ('REGIONAL_MEXICANA'),
-                                       ('RAP'),
-                                       ('CLASICA'),
-                                       ('MOVIE_OST'),
-                                       ('JAZZ'),
-                                       ('COUNTRY'),
-                                       ('POP'),
-                                       ('REGGAETON'),
-                                       ('TRAP_LATINO'),
-                                       ('TRAP'),
-                                       ('K_POP'),
-                                       ('BOLERO'),
-                                       ('PASILLO');
-<<<<<<< HEAD
 
 INSERT INTO Artista (nombre, biografia) VALUES
                                             ('Fleetwood_Mac', 'Banda de rock'),
@@ -74,17 +44,4 @@ INSERT INTO Artista_Genero(id_artista, id_genero) VALUES
                                                       (5,16),
                                                       (6,17),
                                                       (7,6);
-=======
-INSERT INTO Usuario (nombre_usuario, apellido_usuario, correo, contraseña, tipo_usuario, id_foto_Perfil, estado_cuenta) VALUES
-    ('Jonas', 'Garcia', 'jonas@gmail.com', '$2a$10$wFBV8MmGWPzIwsnhJ3/w0eWNici2F8dXx1RUM7F0TxTAa1T2CWfPK', 'ADMINISTRADOR',  '1', 'activo');
-SELECT * FROM Usuario;
-UPDATE Usuario
-SET  estado_cuenta= "activo"
-WHERE correo = "jonas@gmail.com";
-
-
-
-
-
->>>>>>> adcb48c002832e4c9e16b0560b19b794d3ea2ec7
 
