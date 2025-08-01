6586eccbb3c2186d9ceab3845e8a8eaadfae4370
aesir <eduardoarcos6to@gmail.com>
2025-07-31 20:31:10 -0500

    Diagrama de clases actualizado

--- src/BusinessLogic/ComponentePlaylist.java
+++ src/BusinessLogic/ComponentePlaylist.java
@@ -4,8 +4,4 @@ import java.util.*;
 
 public interface ComponentePlaylist {
     void mostrarInformacion();
-    double obtenerDuracion();
-    void agregar(ComponentePlaylist componente);
-    void eliminar(ComponentePlaylist componente);
-    List<ComponentePlaylist> getComponentes();
 }
