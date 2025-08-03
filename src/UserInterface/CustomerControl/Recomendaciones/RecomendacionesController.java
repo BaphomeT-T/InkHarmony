cc16aa87cfb71773313e1a7d9e42329b1f1043de
LeonelXD1 <leonel.onacevallos@epn.edu.ec>
2025-08-03 11:32:04 -0500

    Se agrega botón para regresar al menú anterior

--- src/UserInterface/CustomerControl/Recomendaciones/RecomendacionesController.java
+++ src/UserInterface/CustomerControl/Recomendaciones/RecomendacionesController.java
@@ -3,9 +3,13 @@ package UserInterface.CustomerControl.Recomendaciones;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
 
 public class RecomendacionesController {
 
@@ -17,6 +21,9 @@ public class RecomendacionesController {
     @FXML private Button btnVolver;
 
     @FXML private StackPane contentPane;
+
+    @FXML
+    private Button cerrarButton;
 
     @FXML
     public void initialize() {
@@ -32,6 +39,29 @@ public class RecomendacionesController {
             AnchorPane vista = FXMLLoader.load(getClass().getResource("/UserInterface/GUI/Recomendaciones/" + fxml));
 
             contentPane.getChildren().setAll(vista);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void cerrarVentana(ActionEvent event) {
+        try {
+            // Obtener el Stage actual a partir del botón cerrar
+            Stage stage = (Stage) this.cerrarButton.getScene().getWindow();
+
+            // Cargar el nuevo FXML
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                    "/UserInterface/GUI/AdminUserControl/.fxml")); //Aquí iría la vista del menu principal
+            Parent root = loader.load();
+
+            // Crear la nueva escena con la interfaz de administración
+            Scene scene = new Scene(root);
+
+            // Cambiar la escena del Stage
+            stage.setScene(scene);
+            stage.setTitle("Administración de Usuarios");
+            stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
